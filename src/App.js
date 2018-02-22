15d50107baff924d4d4a9bdd4cdbd5ab88c23aa9
Jens Jákup Egholm Gaardbo <jgaard16@gmail.com>
2018-02-22 23:24:41 +0100

    Changed welcome screen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SCSS from './assets/scss/app.scss';
 import WelcomeMessage from './components/WelcomeMessage.jsx';
 
 const appData = {
-    title: 'React Demo App'
+    title: 'ReactElectron Demo App'
 }
 
 class App extends Component {
@@ -14,11 +14,7 @@ class App extends Component {
         <div className="App-header">
             <WelcomeMessage data={appData}/>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
         </div>
-        <p className="App-intro">
-         Version: 0.1.9
-        </p>
       </div>
     );
   }
